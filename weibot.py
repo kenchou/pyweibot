d79fc4b498d0afa8a168edb81f096ede4c615013
Ken Chou <zhang-j@klab.com>
2014-02-12 18:02:34 +0800

    refactor code

--- weibot.py
+++ weibot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 __author__ = 'kzhang'
@@ -7,29 +8,32 @@ import urllib
 import urllib2
 
 
+
+APP_KEY = '235133751' #你申请的APP_KEY
+APP_SECRET = 'b05603e088a5e9409881c6f4e7a1cfdc' #你申请的APP_SECRET
+#回调地址，可以用这个默认地址
+CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'
+AUTH_URL = 'https://api.weibo.com/oauth2/authorize'
+USER_ID = 'kenchou77@gmail.com' #微博账号
+PASSWORD = 'Tgg6RLzm' #微博密码
+
+
 #模拟授权并且获取回调地址上的code，以获得acces token和token过期的UNIX时间
 def get_code():
-    APP_KEY = 'xxxxxxxxxx' #你申请的APP_KEY
-    APP_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx' #你申请的APP_SECRET
-    #回调地址，可以用这个默认地址
-    CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'
-    AUTH_URL = 'https://api.weibo.com/oauth2/authorize'
-    USERID = 'xxxx@xxx.com' #微博账号
-    PASSWD = 'xxxxxx' #微博密码
 
     client = weibo.APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
     referer_url = client.get_authorize_url()
-    print "referer url is : %s" % referer_url
+    print "authorize url is : %s" % referer_url
 
     cookies = urllib2.HTTPCookieProcessor()
     opener = urllib2.build_opener(cookies)
     urllib2.install_opener(opener)
 
-    postdata = {
+    post_data = {
         "client_id": APP_KEY,
         "redirect_uri": CALLBACK_URL,
-        "userId": USERID,
-        "passwd": PASSWD,
+        "userId": USER_ID,
+        "passwd": PASSWORD,
         "isLoginSina": "0",
         "action": "submit",
         "response_type": "code",
@@ -41,26 +45,20 @@ def get_code():
         "Referer": referer_url
     }
 
-    req = urllib2.Request(
-        url = AUTH_URL,
-        data = urllib.urlencode(postdata),
-        headers = headers
-    )
+    req = urllib2.Request(url=AUTH_URL, data=urllib.urlencode(post_data), headers=headers)
     try:
         resp = urllib2.urlopen(req)
-        #print "callback url is : %s" % resp.geturl()
+
+        print "callback url is : %s" % resp.geturl()
         code = resp.geturl()[-32:]
-        #print "code is : %s" % resp.geturl()[-32:]
+
+        print "code is : %s" % resp.geturl()[-32:]
     except Exception, e:
         print e
     return code
 
 
 def begin():
-
-    APP_KEY = 'xxxxxxxx' # app key
-    APP_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # app secret
-    CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html' # callback url
 
     client = weibo.APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
 
@@ -74,11 +72,11 @@ def begin():
     client.set_access_token(access_token, expires_in)
 
     #发普通微博
-    client.statuses.update.post(status=u'test')
+    client.statuses.update.post(status=u'测试 aaa 23333')
 
     #发图片微博
-    f = open('C:/pic/test.jpg', 'rb')
-    r = client.statuses.upload.post(status=u'测试OAuth 2.0带图片发微博', pic=f)
-    f.close() # APIClient不会自动关闭文件，需要手动关闭
+    # f = open('C:/pic/test.jpg', 'rb')
+    # r = client.statuses.upload.post(status=u'测试OAuth 2.0带图片发微博', pic=f)
+    # f.close() # APIClient不会自动关闭文件，需要手动关闭
 
 begin()
